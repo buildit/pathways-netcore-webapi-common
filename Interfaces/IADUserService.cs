1770d0bba35968ed27ddb9f35532c429deedc8ae
chrispydizzle <chris@cpsharp.net>
2019-05-15 13:37:12 -0400

    rename interface param

--- Interfaces/IADUserService.cs
+++ Interfaces/IADUserService.cs
@@ -2,6 +2,6 @@ namespace pathways_common.Interfaces
 {
     public interface IADUserService<T> : ICrudService<T>, IGetByNameService<T>
     {
-        T RetrieveOrCreate(string adEmail, string dbEmail);
+        T RetrieveOrCreate(string adEmail, string adName);
     }
 }
