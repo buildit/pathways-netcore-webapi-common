18d5faca988cf4a979cdea599198e160551e95c9
chrispydizzle <chris@cpsharp.net>
2019-05-15 12:43:45 -0400

    Move reusable application scaffolding to the common module

--- Core/PathwaysStartup.cs
+++ Core/PathwaysStartup.cs
@@ -1,8 +1,12 @@
 namespace pathways_common.Core
 {
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Logging;
 
     public abstract class PathwaysStartup
     {
@@ -15,15 +19,38 @@ namespace pathways_common.Core
 
         protected abstract void AddEntityFramework(IServiceCollection services);
 
-        protected void ConfigureStandardStack(IServiceCollection services)
+        protected void ConfigurePathwaysServices(IServiceCollection services)
         {
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             this.AddEntityFramework(services);
-            this.AddAdditionalServices(services);
+            this.SetupAzureAdAuth(services);
             services.AddMemoryCache();
         }
 
-        protected abstract void AddAdditionalServices(IServiceCollection services);
+        protected void ConfigurePathways(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                IdentityModelEventSource.ShowPII = true;
+            }
+
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+
+        private void SetupAzureAdAuth(IServiceCollection services)
+        {
+            services
+                .AddAuthentication(sharedOptions => { sharedOptions.DefaultScheme = JwtBearerDefaults.AuthenticationScheme; })
+                .AddJwtBearer(options =>
+                {
+                    options.Audience = this.Configuration["AzureAd:ClientId"];
+                    options.Authority = $"{this.Configuration["AzureAd:Instance"]}{this.Configuration["AzureAd:TenantId"]}";
+                });
+        }
     }
 }
