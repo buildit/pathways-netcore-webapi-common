d3d2e84fd5d8ea1d3b75db40e8679af0895baed7
cp <chris@cpsharp.net>
2019-05-25 07:05:04 -0400

    Skill, types, and type levels now have services and controllers, and accept data from the front-end.

--- Interfaces/Entities/IADUserEntity.cs
+++ Interfaces/Entities/IADUserEntity.cs
@@ -3,7 +3,7 @@ namespace pathways_common.Interfaces.Entities
     public interface IADUserEntity : IIdEntity
     {
         string Username { get; set; }
-        
+
         string DirectoryName { get; set; }
     }
 }
