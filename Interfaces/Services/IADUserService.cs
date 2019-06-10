cb5df2b03eb4458a21a1463c67ab5727973a3647
chrispydizzle <chris@cpsharp.net>
2019-06-10 14:57:51 -0400

    add attendee username to workshop query

--- Interfaces/Services/IADUserService.cs
+++ Interfaces/Services/IADUserService.cs
@@ -2,6 +2,8 @@ namespace pathways_common.Interfaces.Services
 {
     public interface IADUserService<T> : ICrudService<T>, IGetByNameService<T>
     {
+        void SetLogonTime(T user);
+
         T RetrieveOrCreate(string graphEmail, string adEmail, string adName);
     }
 }
